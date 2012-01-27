739f9dfdd151227685133e3f02add84849251e1d
Brad <barkerb1@nku.edu>
2012-01-27 01:40:34 -0500

    Parser / Components
    Work so far on the Parser and Components.  No where near finished but
    enough to give everyone an idea of how they're structured.

---
+++ SMILProject/src/com/csc440/nuf/components/SMILText.java
@@ -0,0 +1,26 @@
+package com.csc440.nuf.components;
+
+import org.xml.sax.Attributes;
+
+/**
+ * CSC-440 SMIL Project
+ * 01-26-2011
+ * SMILText.java
+ * @author Brad Barker
+ * 
+ * 
+ */
+
+public class SMILText extends AbstractSMILObject {
+
+	public SMILText(Attributes att) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void printData() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- SMILProject/src/com/csc440/nuf/parser/SMILParser.java
+++ SMILProject/src/com/csc440/nuf/parser/SMILParser.java
@@ -10,33 +10,72 @@ package com.csc440.nuf.parser;
  * 
  */
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
 public class SMILParser {
-	
-	SMILParser(URL SMILFile){
-		try{
-			
-			SAXParserFactory spf = SAXParserFactory.newInstance();
-			SAXParser sp = spf.newSAXParser();
-			XMLReader xReader = sp.getXMLReader();
-			SMILHandler sHandler = new SMILHandler();
-			
-			xReader.setContentHandler(sHandler);
-			xReader.parse(new InputSource(SMILFile.openStream()));
-			
-			
-		}
-		catch(Exception e){
-			//Bad URL
-		}
-		
-	}
 
+    private InputStream is;
+
+    //If connection passes a URL
+    SMILParser(URL SMILURL) {
+        try {
+            is = SMILURL.openStream();
+            parse();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(SMILParser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    //If connection passes a file
+    SMILParser(File SMILFile) {
+        try {
+            is = new FileInputStream(SMILFile);
+            parse();
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(SMILParser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private void parse() {
+
+        try {
+
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            SAXParser sp = spf.newSAXParser();
+            XMLReader xReader = sp.getXMLReader();
+            SMILHandler sHandler = new SMILHandler();
+
+            xReader.setContentHandler(sHandler);
+            xReader.parse(new InputSource(is));
+
+
+        } catch (ParserConfigurationException e) {
+            //Bad ParserFactory
+        } catch (SAXException e) {
+            //Bad SAXParser
+        } catch (IOException e) {
+            //Bad XMLReader
+        }
+
+
+    }
 }
+
