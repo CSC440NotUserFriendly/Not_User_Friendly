2045a3c88ef6560f8b20d5161e5d0970f4f1ff4e
Brad <barkerb1@nku.edu>
2012-01-26 23:11:50 -0500

    Object Shells
    Created the shells for the various SMIL components.  Will finish their
    implementation NLT 1-29.

---
+++ SMILProject/src/com/csc440/nuf/SMILAudio.java
@@ -0,0 +1,21 @@
+package com.csc440.nuf;
+
+/**
+ * CSC-440 SMIL Project
+ * 01-26-2011
+ * SMILAudio.java
+ * @author Brad Barker
+ * 
+ * 
+ */
+
+
+public class SMILAudio extends AbstractSMILObject {
+
+	@Override
+	public void printData() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SMILProject/src/com/csc440/nuf/SMILParallel.java
@@ -0,0 +1,14 @@
+package com.csc440.nuf;
+
+/**
+ * CSC-440 SMIL Project
+ * 01-26-2011
+ * SMILParallel.java
+ * @author Brad Barker
+ * 
+ * 
+ */
+
+public class SMILParallel {
+
+}
