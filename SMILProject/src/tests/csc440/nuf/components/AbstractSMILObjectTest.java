000c5d1d46b7b23a7921bd7caae081227dee02ec
Alex Gilbert <agilbert7911@gmail.com>
2012-02-09 17:16:14 -0500

    removed useless tests modified status update and added a new test

---
+++ SMILProject/src/tests/csc440/nuf/components/AbstractSMILObjectTest.java
@@ -0,0 +1,35 @@
+
+package tests.csc440.nuf.components;
+
+import com.csc440.nuf.components.*;
+
+import junit.framework.TestCase;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.AttributesImpl;
+
+/**
+ * CSC-440 SMIL Project
+ * 02-09-2011
+ * AbstractSMILObjectTest.java
+ * @author Alex Gilbert
+ * 
+ */
+
+class AbstractSMILObjectTest extends TestCase{
+	
+	private AttributesImpl att;
+	private SMILAudio sa;
+	/* 
+	 * 
+	 */
+	@BeforeClass
+	public void setUp(){
+	    att = new AttributesImpl();
+		att.addAttribute("testUri","testLocalName","testQName","testType","testValue");
+		sa = new SMILAudio(att);
+	}
+}
